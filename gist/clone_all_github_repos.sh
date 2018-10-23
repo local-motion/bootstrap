b4d4af6f22241b84c2cfa7b26cec2fbdff364998
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-10-23 13:13:54 +0200

    Script for locally deploying all services

--- gist/clone_all_github_repos.sh
+++ gist/clone_all_github_repos.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-ORGANIZATION=$1
-
 info() { printf "\\033[38;5;040mℹ\\033[0m %s\\n" "$1"; }
 error() { printf "\\033[38;5;124m✗\\033[0m %s\\n" "$1"; }
 debug() { printf "\\033[38;5;033m✓\\033[0m %s\\n" "$1"; }
